631de3a40a5e71d77ea8630e8cf0a87f67660745
Luis Armando Tejada Murillo <luartemu@gmail.com>
2023-09-21 19:02:42 -0600

    chore: first commit

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Define your collection(s)
+const postCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      date: z.string(),
+      image: image(),
+      title: z.string(),
+    }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  posts: postCollection,
+};
